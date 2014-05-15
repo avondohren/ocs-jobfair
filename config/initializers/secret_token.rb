d392264513cee257cef5f04df108deb98c968f59
Andy von Dohren <avondohren@gmail.com>
2014-05-15 08:20:21 -0500

    initial build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OcsCheckin::Application.config.secret_token = 'c317087cbe536960364d2ae6c0823645e537dad90c92d7a77705ac861bddbb69b75c6799b29e8130453afaf40c0d6345ef38f326622859d2ad6990792059ce61'
